9e3a79c88b280544e5d4d93c107f58147fb98adc
strk2333 <strk23@outlook.com>
2017-09-20 16:02:23 +0800

    Zen comes from fabrication.

--- code2017/kyu.py
+++ code2017/kyu.py
@@ -90,7 +90,7 @@ class MFuns:
 
         ai_count_list = []
         limit_copy = 0
-        for i in range(0, len(residue_list)):
+        for i in range(len(residue_list)):
             if residue_list[i] is not None:
                 residue_list_copy = residue_list.copy()
                 limit_copy = limit
@@ -118,15 +118,122 @@ class MFuns:
         else:
             return max(ai_count_list)
 
-    def is_all_negative(self, residue_list):
-        for i in residue_list:
-            if i is not None and int(i) > 0:
-                return False
-        return True
+    """
+    最舒适的路线
+    时间限制：5000 ms  |  内存限制：65535 KB
+    难度：5
+    描述
+    异形卵潜伏在某区域的一个神经网络中。其网络共有N个神经元（编号为1,2,3,…,N），这些神经元由M条通道连接着。
+    两个神经元之间可能有多条通道。异形卵可以在这些通道上来回游动，但在神经网络中任一条通道的游动速度必须是一定的。
+    当然异形卵不希望从一条通道游动到另一条通道速度变化太大，否则它会很不舒服。现在异形卵聚居在神经元S点，
+    想游动到神经元T点。它希望选择一条游动过程中通道最大速度与最小速度比尽可能小的路线，也就是所谓最舒适的路线。
+    
+    输入
+    第一行： K 表示有多少组测试数据。 
+    接下来对每组测试数据：
+    第1行: N M
+    第2~M+1行： Xi Yi Vi (i=1,…..,M)
+    表示神经元Xi 到神经元Yi之间通道的速度必须是Vi
+    最后一行： S T ( S  T )
+    
+    【约束条件】
+    2≤K≤5 1<N≤500 0<M≤5000 1≤ Xi, Yi , S , T ≤N 0< Vi <30000，
+    Vi是整数。数据之间有一个空格。
+    输出
+    对于每组测试数据，输出一行：如果神经元S到神经元T没有路线，输出“IMPOSSIBLE”。否则输出一个数，表示最小的速度比。
+    如果需要，输出一个既约分数。
+    样例输入
+    2
+    3 2
+    1 2 2
+    2 3 4
+    1 3
+    3 3
+    1 2 10
+    1 2 5
+    2 3 8
+    1 3
+    样例输出
+    2
+    5/4
+    """
+    def alien_route(self):
+        input1 = int(input("Input the amount of test cases: "))
+
+        for i in range(input1):
+            input2 = input("Input the amount of aliens routes: ")
+            alien_count = int(input2.split(' ')[0])
+            route_count = int(input2.split(' ')[1])
+
+            route_list = []
+            for j in range(route_count):
+                input3 = input("Input the route: ")
+                route_list.append([int(input3.split(' ')[0]), int(input3.split(' ')[1]), int(input3.split(' ')[2])])
+
+            input4 = input("Input source & destination: ")
+            s = int(input4.split(' ')[0])
+            t = int(input4.split(' ')[1])
+
+            self.alien_best_route(alien_count, route_count, route_list, s, t)
+
+        pass
+
+    def alien_best_route(self, alien_count, route_count, route_list, s, t):
+
+        pass
+
+    """
+    Travel in time
+    时间限制：4000 ms  |  内存限制：65535 KB
+    难度：5
+    描述
+    Bob gets tired of playing games, leaves Alice, and travels to Changsha alone. Yuelu Mountain, Orange Island, Window 
+    of the World, the Provincial Museum etc...are scenic spots Bob wants to visit. However, his time is very limited, 
+    he can’t visit them all.
+    Assuming that there are N scenic spots in Changsha, Bob defines a satisfaction value Si to each spot. 
+    If he visits this spot, his total satisfaction value will plus Si. Bob hopes that within the limited time T, 
+    he can start at spot S, visit some spots selectively, and finally stop at spot E, so that 
+    the total satisfaction value can be as large as possible. It's obvious that visiting the spot will also cost 
+    some time, suppose that it takes Ci units of time to visit spot i ( 0 <= i < N ).
+    Always remember, Bob can choose to pass by a spot without visiting it (including S and E), maybe he just want to 
+    walk shorter distance for saving time.
+    Bob also has a special need which is that he will only visit the spot whose satisfaction value is strictly larger 
+    than that of which he visited last time. For example, if he has visited a spot whose satisfaction value is 50, 
+    he would only visit spot whose satisfaction value is 51 or more then. The paths between the spots are 
+    bi-directional, of course.
+    输入
+    The first line is an integer W, which is the number of testing cases, and the W sets of data are following.
+    The first line of each test data contains five integers: N M T S E. N represents the number of spots, 1 < N < 100; 
+    M represents the number of paths, 0 < M < 1000; T represents the time limitation, 0 < T <= 300; S means the spot 
+    Bob starts from. E indicates the end spot. (0 <= S, E < N)
+    The second line of the test data contains N integers Ci ( 0 <= Ci <= T ), which means the cost of time 
+    if Bob visits the spot i.
+    The third line also has N integers, which means the satisfaction value Si that can be obtained by visiting 
+    the spot i ( 0 <= Si < 100 ).
+    The next M lines, each line contains three integers u v L, means there is a bi-directional path between spot u 
+    and v and it takes L units of time to walk from u to v or from v to u. (0 <= u, v < N, 0 <= L <= T)
+    输出
+    Output case number in the first line (formatted as the sample output).
+    The second line contains an integer, which is the greatest satisfaction value.
+    If Bob can’t reach spot E in T units of time, you should output just a “0” (without quotation marks).
+    样例输入
+    1
+    4 4 22 0 3
+    1 1 1 1
+    5 7 9 12
+    0 1 10
+    1 3 10
+    0 2 10
+    2 3 10
+    样例输出
+    Case #1:
+    21
+    """
 
     def run(self):
         #MFuns.shuffling_card(self)
-        MFuns.ant6(self)
+        #MFuns.ant6(self)
+        MFuns.alien_route(self)
         pass
 
     pass
